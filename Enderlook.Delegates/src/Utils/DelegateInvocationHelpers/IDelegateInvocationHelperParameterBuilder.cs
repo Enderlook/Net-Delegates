284387bc5c849a44fdc5755ed5b04f7aa9b8681f
Enderlook <enderlook@gmail.com>
2025-05-11 11:48:50 -0300

    Use more internal

--- Enderlook.Delegates/src/Utils/DelegateInvocationHelpers/IDelegateInvocationHelperParameterBuilder.cs
+++ Enderlook.Delegates/src/Utils/DelegateInvocationHelpers/IDelegateInvocationHelperParameterBuilder.cs
@@ -5,17 +5,17 @@
 /// </summary>
 public interface IDelegateInvocationHelperParameterBuilder
 {
-    public int ParametersCount { get; }
+    internal int ParametersCount { get; }
 
-    public bool AcceptsParameterType(int i, Type type);
+    internal bool AcceptsParameterType(int i, Type type);
 
-    public T GetParameter<T>(int i)
+    internal T GetParameter<T>(int i)
 #if NET9_0_OR_GREATER
         where T : allows ref struct
 #endif
         ;
 
-    public T? TryGetParameter<T>(int i, out bool can)
+    internal T? TryGetParameter<T>(int i, out bool can)
 #if NET9_0_OR_GREATER
         where T : allows ref struct
 #endif
